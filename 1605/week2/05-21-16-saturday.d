07776fd1ed5254b334085039e78e209df5d9ba51
thompickett <thompickett@gmail.com>
2016-05-21 09:41:26 -0600

    saturday agenda

---
+++ 1605/week2/05-21-16-saturday.d
@@ -0,0 +1,34 @@
+# Saturday 5-21-2016 Pahlka Posse Meeting Agenda
+
+##### timekeeper: 
+##### notekeeper: 
+
+### Kickoff @ 11:00
+
+* 11:05-11:15 Recap on progress from yesterday
+* 11:15-11:25 Go over teams make adjustements if necessary
+
+### Work Time
+* 11:25-01:00 Work Time, take breaks as needed
+* 01:00-01:10 Recap on progress from this morning
+* 01:40-02:50 Work Time, take breaks as needed
+
+### Recap
+* 02:50-03:00 Recap on the day!!
+
+### Groups 
+ * mapping work  
+  `July`  
+  `Patrick`  
+  `Karina`  
+ * rails work google api(this might be finished)  
+  `nonaps`   
+  `Sonia`    
+ * rails work creating methods for calculations using presenters, or a model  
+  `Thom`  
+  `Kerry`  
+  
+### Notes
+
+
+
